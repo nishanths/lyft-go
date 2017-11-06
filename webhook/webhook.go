3f9079b0574fc6b1f7cb21b941b8226f86f41e1f
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2017-11-06 07:43:14 -0500

    convenience webhook decode func

--- webhook/webhook.go
+++ webhook/webhook.go
@@ -3,10 +3,14 @@
 package webhook // import "go.avalanche.space/lyft/webhook"
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"time"
@@ -65,15 +69,69 @@ func (e *Event) UnmarshalJSON(p []byte) error {
 	return nil
 }
 
-// Signature returns the value of "X-Lyft-Signature" in the header.
-// The "sha256=" prefix will have been trimmed.
+// Signature returns the value of "X-Lyft-Signature" from an incoming
+// webhook request header. The "sha256=" prefix will have been trimmed
+// in the returned string.
 func Signature(h http.Header) string {
 	return strings.TrimPrefix(h.Get("X-Lyft-Signature"), "sha256=")
 }
 
-func VerifySignature(requestBody, signature, verificationToken []byte) bool {
+// Verify checks whether the incoming webhook requests originated from
+// Lyft. The signature is the value of the X-Lyft-Signature header value
+// with the "sha256=" prefix already trimmed. The verification token
+// can be be found in the Lyft Developer Portal.
+func Verify(requestBody, signature, verificationToken []byte) (bool, error) {
 	mac := hmac.New(sha256.New, verificationToken)
-	mac.Write(requestBody)
-	bodySignature := mac.Sum(nil)
-	return hmac.Equal([]byte(base64.StdEncoding.EncodeToString(bodySignature)), signature)
+	_, err := mac.Write(requestBody)
+	if err != nil {
+		return false, err
+	}
+	expectedMAC := mac.Sum(nil)
+
+	// Base64 encode the body MAC.
+	var buf bytes.Buffer
+	enc := base64.NewEncoder(base64.StdEncoding, &buf)
+	_, err = enc.Write(expectedMAC)
+	if err != nil {
+		return false, err
+	}
+	err = enc.Close()
+	if err != nil {
+		return false, err
+	}
+
+	return hmac.Equal(buf.Bytes(), signature), nil
+}
+
+var ErrVerify = errors.New("failed to verify request")
+
+// DecodeEvents decodes an incoming webhook request's body and header
+// into an Event. DecodeEvent will verify that the request was received from
+// Lyft. The error will be ErrVerify if verification fails.
+// The request body will always be drained and closed, even if an error is returned.
+// Errors from draining and closing are not reported.
+func DecodeEvent(requestBody io.ReadCloser, h http.Header, verificationToken []byte) (Event, error) {
+	defer drainAndClose(requestBody)
+	var e Event
+
+	// Create copies of required readers.
+	var verifyBuf bytes.Buffer // copy of buffer to use in verification
+	if _, err := verifyBuf.ReadFrom(requestBody); err != nil {
+		return e, err
+	}
+	decodeBuf := bytes.NewBuffer(verifyBuf.Bytes()) // for JSON decoding Event
+
+	if ok, err := Verify(verifyBuf.Bytes(), []byte(Signature(h)), verificationToken); err != nil {
+		return e, err // generic internal error
+	} else if !ok {
+		return e, ErrVerify // verification failed
+	}
+
+	err := json.NewDecoder(decodeBuf).Decode(&e)
+	return e, err
+}
+
+func drainAndClose(r io.ReadCloser) {
+	io.Copy(ioutil.Discard, r)
+	r.Close()
 }
